6fa73688bde7d02bb52718a5435cad01d164a26a
Davi TS Junior <dv_junior@hotmail.com>
2022-06-13 16:55:28 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/teste/koerich/JogoCartasApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.koerich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoCartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoCartasApplication.class, args);
+	}
+
+}
